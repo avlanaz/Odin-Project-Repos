f277f5024818620fa141d9d4ff55d22b54ba7cce
avlanaz <haflanaziz@gmail.com>
2022-11-23 22:30:05 +1100

    Rearrange contact page

--- Restaurant/src/contact.js
+++ Restaurant/src/contact.js
@@ -1,15 +1,31 @@
+const babaURL = "https://images.unsplash.com/photo-1602289313886-60b6cd71b3dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1885&q=80";
+const sefURL = "https://images.pexels.com/photos/3338540/pexels-photo-3338540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+const teyzeURL = "https://images.pexels.com/photos/14425947/pexels-photo-14425947.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
 var contactPage = (`
 <h1>Contact Us</h1>
 
 <div class="card-container">
     <div class="card">
-        <p>Baba Sahip <br/> 0301-000-000 </p>
+        <div class="contact-detail">
+            <h1>Ser Sahip</h1>
+            <p>0301-xxx-xxx</p>
+        </div>
+        <img src=${babaURL} />
     </div>
     <div class="card">
-        <p>Sef Efendi <br/> 0301-000-000 </p>
+        <div class="contact-detail">
+            <h1>Baba Chef</h1>
+            <p>0301-xxx-xxx</p>
+        </div>
+        <img src=${sefURL} />
     </div>
     <div class="card">
-        <p>Teyze Garson <br/> 0301-000-000 </p>
+        <div class="contact-detail">
+            <h1>Auntie Garson</h1>
+            <p>0301-xxx-xxx</p>
+        </div>
+        <img src=${teyzeURL} />
     </div>
 </div>
 `)
