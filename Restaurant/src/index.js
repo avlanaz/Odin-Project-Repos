55d91867ed9a3695397500a65118dfbab4848ceb
avlanaz <haflanaziz@gmail.com>
2022-11-23 16:25:24 +1100

    Set up buttons with page changing function on click

--- Restaurant/src/index.js
+++ Restaurant/src/index.js
@@ -11,49 +11,31 @@ var contactButton;
 var pageContainer;
 
 // Helper to arrange page functions
-const getPage = {
+const pages = {
     "home": homePage,
     "menu": menuPage,
     "contact": contactPage
 };
 
 initWebPage();
+setupTabButtons();
 loadPage("home");
 
 function loadPage(idPage) {
+    console.log("loadingpage")
     // Get page according to the "idPage"
-    const page = getPage[idPage];
+    const page = pages[idPage];
 
     // Then make contentContainer to display that page
     const pageContainer = document.getElementById("content-card");
     pageContainer.innerHTML = page;
 }
 
-function initPage() {
-    // Set up the DOM functions
-    homeButton = document.getElementById("home-button");
-    menuButton = document.getElementById("menu-button");
-    contactButton = document.getElementById("contact-button");
-    
-    pageContainer = document.getElementById("page-container");
-    
-    homeButton.addEventListener('click', () => {
-        renderPage(homePage);
-    })
-    var homeButton;
-    var menuButton;
-    var contactButton;
-    var pageContainer;
-    
-    menuButton.addEventListener('click', () => {
-        renderPage(menuPage);
-    })
-    
-    contactButton.addEventListener('click', () => {
-        renderPage(contactPage);
-    })
-
-    function renderPage(page) {
-        pageContainer.innerHTML = page;
-    }
+function setupTabButtons() {
+    for (const page in pages) {
+        const pageButton = document.getElementById(`${page}-button`);
+        pageButton.addEventListener("click", () => {
+            loadPage(page);
+        });
+    };
 }
